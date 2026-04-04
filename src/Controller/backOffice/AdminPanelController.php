c9ff96a8d98448e3311df70faac8790fe968cff1
alaahedfi <alaahefi123@gmail.com>
2026-04-05 00:37:09 +0100

    admin panel cont

--- src/Controller/backOffice/AdminPanelController.php
+++ src/Controller/backOffice/AdminPanelController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\backOffice;
 
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
