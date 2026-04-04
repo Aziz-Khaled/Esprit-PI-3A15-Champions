587d351dde3173109b3cd15b432fc9068010b847
alaahedfi <alaahefi123@gmail.com>
2026-04-05 00:41:15 +0100

    cart contr

--- src/Controller/backOffice/AdminPanelController.php
+++ src/Controller/backOffice/AdminPanelController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller\backOffice;
 
-
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
