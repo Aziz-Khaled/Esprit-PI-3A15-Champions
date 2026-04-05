55103645065058a0b5299872bc37c760a4a96df9
Eya-bouraoui <Eya.bouraoui2005@gmail.com>
2026-04-05 02:14:42 +0100

    real-time search

--- src/Controller/frontOffice/TransactionController.php
+++ src/Controller/frontOffice/TransactionController.php
@@ -46,16 +46,28 @@ class TransactionController extends AbstractController
     }
 
     /**
-     * Action pour afficher toutes les transactions (Côté Admin)
+     * Action pour afficher et filtrer les transactions (Côté Admin)
      */
-    #[Route('/admin/transactions', name: 'app_admin_transactions', methods: ['GET'])]
-    public function index(TransactionRepository $transactionRepository): Response
+    #[Route('/admin_panel/transactions', name: 'app_admin_transactions', methods: ['GET'])]
+    public function adminTransactions(Request $request, TransactionRepository $repo): Response
     {
-        // CORRECTION : Utilisation de 'dateTransaction' (propriété PHP) et non 'date_transaction' (colonne SQL)
-        $transactions = $transactionRepository->findBy([], ['dateTransaction' => 'DESC']);
+        // Récupération des paramètres de filtrage depuis la requête GET
+        $userName = $request->query->get('user_name');
+        $type = $request->query->get('type');
 
+        // Si des filtres sont présents, on utilise la méthode personnalisée
+        if ($userName || $type) {
+            $transactions = $repo->findByFilters($userName, $type);
+        } else {
+            // Sinon, on affiche tout par ordre décroissant (comportement par défaut)
+            $transactions = $repo->findBy([], ['dateTransaction' => 'DESC']);
+        }
+
+        // Vérifie que le chemin du template correspond bien à ton dossier (admin/ ou admin_panel/)
         return $this->render('admin_panel/transactions.html.twig', [
             'transactions' => $transactions,
+            'current_user_name' => $userName, // Pour garder la valeur dans l'input après validation
+            'current_type' => $type           // Pour garder la sélection dans le dropdown
         ]);
     }
 }
--- src/Repository/TransactionRepository.php
+++ src/Repository/TransactionRepository.php
@@ -16,6 +16,39 @@ class TransactionRepository extends ServiceEntityRepository
         parent::__construct($registry, Transaction::class);
     }
 
+    /**
+     * Recherche dynamique Nom/Prénom et Type
+     */
+    public function findByFilters(?string $userName, ?string $type): array
+    {
+        $qb = $this->createQueryBuilder('t')
+            ->leftJoin('t.walletSource', 'ws')
+            ->leftJoin('ws.utilisateur', 'u')
+            ->leftJoin('t.creditCard', 'cc')
+            ->leftJoin('cc.utilisateur', 'u2');
+
+        if ($userName) {
+            // CONCAT permet de chercher sur "Nom Prénom" ou "Prénom Nom" en une seule fois
+            $qb->andWhere(
+                $qb->expr()->orX(
+                    'CONCAT(u.prenom, \' \', u.nom) LIKE :name',
+                    'CONCAT(u.nom, \' \', u.prenom) LIKE :name',
+                    'CONCAT(u2.prenom, \' \', u2.nom) LIKE :name',
+                    'CONCAT(u2.nom, \' \', u2.prenom) LIKE :name'
+                )
+            )->setParameter('name', '%' . $userName . '%');
+        }
+
+        if ($type) {
+            $qb->andWhere('t.type = :type')
+               ->setParameter('type', $type);
+        }
+
+        return $qb->orderBy('t.dateTransaction', 'DESC')
+                  ->getQuery()
+                  ->getResult();
+    }
+
 //    /**
 //     * @return Transaction[] Returns an array of Transaction objects
 //     */
